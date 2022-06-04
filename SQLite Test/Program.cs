18238c28f539f6f543e633ef3a7b3c4db186ae5d
Myricht <kennethvanmele@hotmail.com>
2022-06-04 16:37:32 +0200

    Test1
    Firts mock from tutolrial I found online.

--- SQLite Test/Program.cs
+++ SQLite Test/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SQLite;
+
+string cs = @"URI=file:D:\OneDrive - Xylos NV\Documenten\repos\SQLite Test\test.db";
+
+using var con = new SQLiteConnection(cs);
+con.Open();
+
+using var cmd = new SQLiteCommand(con);
+
+cmd.CommandText = "DROP TABLE IF EXISTS cars";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = @"CREATE TABLE cars(id INTEGER PRIMARY KEY, name TEXT, price INT)";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Audi',52642)";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Mercedes',57127)";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Skoda',9000)";
+cmd.ExecuteNonQuery();
+
+string stm = "SELECT * FROM cars LIMIT 5";
+
+using var cmd1 = new SQLiteCommand(stm, con);
+using SQLiteDataReader rdr = cmd1.ExecuteReader();
+
+while (rdr.Read())
+{
+    Console.WriteLine($"{rdr.GetInt32(0)} {rdr.GetString(1)} {rdr.GetInt32(2)}");
+}
