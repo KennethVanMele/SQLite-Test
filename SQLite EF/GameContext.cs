b5802cb231e7a6dea4056a79145fa4482aefc664
Myricht <kennethvanmele@hotmail.com>
2022-06-04 20:57:01 +0200

    WIP
    Find out how to get only PlatformID

---
+++ SQLite EF/GameContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SQLite_EF
+{
+    public class GameContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
+
+        public string DbPath { get; }
+
+        public GameContext()
+        {
+            var folder = Environment.SpecialFolder.MyDocuments;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "repos\\SQLite Test\\test_EF.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
---
+++ SQLite EF/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace SQLite_EF
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            using var db = new GameContext();
+            
+            db.Database.EnsureCreated();
+
+            db.Add(new Platform() { PlatformName = "TestP1" });
+
+
+            //var PlatformID = db.Platforms.Where(x => x.PlatformName == "TestP1");
+            var PlatformID = db.Platforms.Select(x => x.PlatformName == "TestP1");
+            Console.WriteLine(PlatformID);
+
+            var g = new Game
+            {
+                Title = "Test1"
+                //PlatformID = PlatformID
+            };
+        }
+    }
+}
